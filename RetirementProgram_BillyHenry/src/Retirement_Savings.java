50a4519ca63abe00e25ce9c280e3adeccb3eae07
whenry97 <whenry97@192.168.0.158>
2018-10-28 19:45:03 -0400

    try/catch updates made on 10/28

--- RetirementProgram_BillyHenry/src/Retirement_Savings.java
+++ RetirementProgram_BillyHenry/src/Retirement_Savings.java
@@ -3,10 +3,17 @@
 //File: Retirement_Savings.java
 //Description: Calculates total amount of user's savings by retirement.
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class Retirement_Savings {
+
+	//initialize variables to be used
+	static int yearsToRetirement = 0;
+	static double annualSavings = 0;
+	static double interestRate = 0.05;
+	static String totalSaved;
 	
 	public static void main(String[] args) 
 	{
@@ -14,33 +21,48 @@ public class Retirement_Savings {
 		//Create scanner input method
 		Scanner userInput = new Scanner(System.in);
 		
-		//initialize variables to be used
-		int yearsToRetirement;
-		double annualSavings;
-		double interestRate = 0.05;
-		double totalSaved = 0;
+		//try for invalid type entry
+		try {
 		
+		//try for invalid retirement year
+		try {
+			
 		System.out.println("How many years until you retire?: ");
 		yearsToRetirement = userInput.nextInt();
+		retirementYearWithException(yearsToRetirement);
+		} //end of try for invalid retirement year.
 		
-		//Error checking
-		while(yearsToRetirement <= 0 && yearsToRetirement < 100)
+		catch(InputMismatchException e2) 
 		{
-			System.out.println("Error: please enter a number greater than 0");
-			System.out.println("How many years until you retire?: ");
-			yearsToRetirement = userInput.nextInt();
-		} 
+			System.err.println("Error: input for years to retirement must be between 0 and 120.");
+			System.err.println("Program has terminated.");
+			System.exit(0);
+		}
+		
+		//try for invalid savings amount
+		try {
 		
 		System.out.println("How much money are you able to save per year?: ");
 		annualSavings = userInput.nextDouble();
+		annualSavingsWithException(annualSavings);
+		} //end of try for invalid savings amount
 		
-		//Error checking
-		while(annualSavings <= 0)
+		catch(InputMismatchException e3)
 		{
-			System.out.println("Error: please enter a number greater than 0");
-			System.out.println("How much money are you able to save per year?: ");
-			annualSavings = userInput.nextDouble();
-		} 
+			System.err.println("Error: input for savings amount must be greater than $1.00");
+			System.err.println("Program has terminated.");
+			System.exit(0);
+		}
+		
+		} //end of try for invalid type input.
+		
+		catch(InputMismatchException e1) 
+		{
+			e1.printStackTrace();
+			System.err.println("Error: input value must be a number.");
+			System.err.println("Program has terminated.");
+			System.exit(0);
+		} //catch to throw exception if char is entered instead of string.
 		
 		//Call on Savings method to perform calculation
 		totalSaved = Savings(annualSavings, yearsToRetirement, interestRate);
@@ -50,7 +72,7 @@ public class Retirement_Savings {
 		userInput.close();	
 	}
 	
-	public static double Savings(double amount, int years, double rate)
+	public static String Savings(double amount, int years, double rate)
 	{
 		//Performs calculation to find total savings with interest
 		double total, interest;
@@ -63,8 +85,26 @@ public class Retirement_Savings {
 			total += amount + interest;
 		}
 		
-		total = Double.parseDouble(new DecimalFormat("##.00").format(total));
+		DecimalFormat df = new DecimalFormat("##.00");
+		String roundedTotal = df.format(total);
 		
-		return total;
+		return roundedTotal;
 	}
+	
+	public static double retirementYearWithException(double yearsToRetirment)
+	{
+		if(yearsToRetirement < 0 || yearsToRetirement > 120)
+			throw new InputMismatchException("Must retire in greater than 0 and less than 120 years.");
+			
+		return(yearsToRetirement);
+	}
+	
+	public static double annualSavingsWithException(double annualSavings)
+	{
+		if(annualSavings < 1.00)
+			throw new InputMismatchException("Must save more than $1.00 per year.");
+		
+		return(annualSavings);
+	}
+	
 }
